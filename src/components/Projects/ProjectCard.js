73b303b1a516a77e1a55606ae2c708c0f186192b
Joshua Jainga <jjainga@gmail.com>
2020-11-15 18:23:26 -0800

    added sections and fixed typos

--- src/components/Projects/ProjectCard.js
+++ src/components/Projects/ProjectCard.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React,{useState, useEffect} from "react"
 import LanguageList from "./languageList"
 
 //import images
@@ -19,6 +19,27 @@ import GoogleBooks from "./GoogleBooks.PNG"
 
 
 const ProjectCard = (props) => {
+console.log(props)
+
+    const [pinnedState, setPinnedState] = useState({})
+    const [repoState, setRepoState] = useState({})
+
+    async function sortPinned(){
+        let pin = []
+        let notPin = []
+        await props.props.map(pinObj => {
+            pinObj.stargazers_count > 0 ? 
+            pin.push(pinObj) 
+            : 
+            notPin.push(pinObj)
+        })
+        setPinnedState({pinned: pin})
+        setRepoState({repo: notPin})
+    }
+
+    useEffect( () => {
+        sortPinned();
+    },[props])
 
     function thumbnail(value) {
        switch(value) {
@@ -58,19 +79,47 @@ const ProjectCard = (props) => {
                 return "https://i1.wp.com/gchs.org/wp-content/uploads/Image-Coming-Soon-Placeholder.png?resize=350%2C240&ssl=1"
        }}
                    
-    return (
-    props.props.map(projectObj => 
-                <div className="card col-3 ProjectCard text-center m-1 p-1" key={projectObj.id}>
-                    {console.log(projectObj.name)}
+    return(
+    
+      
+   
+       repoState.repo != undefined ?
+       <div className="d-inline-block">
+           <h1>Favorites</h1>
+        <div className="d-inline-block m-auto">
+            {pinnedState.pinned.map(projectObj => 
+                <div className="card col-3 ProjectCard text-center m-1 p-1 d-inline-block" key={projectObj.id}>
                     <img className="card-img-top border" src={thumbnail(projectObj.name)} alt={projectObj.name} />
-                <div className="card-body">
-                    <h5 className="card-title">{projectObj.name}</h5>
-                    <a href={projectObj.homepage || projectObj.html_url} className="btn btn-primary mr-1">Check it out!</a>
-                    <a href={projectObj.html_url} className="btn btn-primary ml-1">Github Repo!</a>
-                    <p className="card-text">Description: {projectObj.description}</p>
-                    <LanguageList props={projectObj.ProjLanguages}/>
-                </div>
-                </div>
-    ))
+                    <div className="card-body">
+                        <h5 className="card-title">{projectObj.name}</h5>
+                        <a href={projectObj.homepage || projectObj.html_url} className="btn btn-primary mr-1">Check it out!</a>
+                        <a href={projectObj.html_url} className="btn btn-primary ml-1">Github Repo!</a>
+                        <p className="card-text">Description: {projectObj.description}</p>
+                        <LanguageList props={projectObj.ProjLanguages}/>
+                    </div>
+                </div>)}
+        </div>
+        <hr />
+        <h1>Working Projects</h1>
+        <div className="d-inline-block m-auto">
+            {repoState.repo.map(notfavObj => 
+                <div className="card col-3 ProjectCard text-center m-1 p-1 d-inline-block" key={notfavObj.id}>
+                    <img className="card-img-top border" src={thumbnail(notfavObj.name)} alt={notfavObj.name} />
+                    <div className="card-body">
+                        <h5 className="card-title">{notfavObj.name}</h5>
+                        <a href={notfavObj.homepage || notfavObj.html_url} className="btn btn-primary mr-1">Check it out!</a>
+                        <a href={notfavObj.html_url} className="btn btn-primary ml-1">Github Repo!</a>
+                        <p className="card-text">Description: {notfavObj.description}</p>
+                        <LanguageList props={notfavObj.ProjLanguages}/>
+                    </div>
+                </div>)}
+        </div>
+        </div>
+                : null
+   
+    
+    )
+    
+            
 }
 export default ProjectCard;
